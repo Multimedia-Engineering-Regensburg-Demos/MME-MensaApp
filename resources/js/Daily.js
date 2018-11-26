513e792a96ce8a7bcbfee118947d33071c6b00be
Alexander Bazo <alexanderbazo@googlemail.com>
2018-11-26 10:38:20 +0100

    Add solution

--- resources/js/Daily.js
+++ resources/js/Daily.js
@@ -4,10 +4,39 @@ var Daily = Daily || {};
 Daily.App = (function() {
   "use strict";
 
-  var that = {};
+  var that = {},
+    menu,
+    view;
 
   function init() {
-    console.log("in: Daily.App.init()");
+    initUI();
+    initMenu();
+    menu.update();
+  }
+
+  function initUI() {
+    let daySelector = document.querySelector(".day-selector"),
+      menuEl = document.querySelector(".daily-menu");
+    view = new Daily.ViewController().init(daySelector, menuEl);
+    view.addEventListener("daySelected", onDaySelected);
+  }
+
+  function initMenu() {
+    menu = new Daily.DataProvider();
+    menu.addEventListener("onMenuUpdate", onMenuUpdateAvailable);
+  }
+
+  function showMenuFor(day) {
+    let dailyMenu = menu.getDailyMenuFor(day);
+    view.renderMenu(day, dailyMenu);
+  }
+
+  function onDaySelected(event) {
+    showMenuFor(event.day);
+  }
+
+  function onMenuUpdateAvailable() {
+    showMenuFor("monday");
   }
 
   that.init = init;
