e392c5157735ceba2f961cb788c1211cd4efc5e2
Alexander Bazo <alexanderbazo@googlemail.com>
2020-05-19 20:57:59 +0200

    Fix error where empty API result would halt app start

--- resources/js/data/DataManager.js
+++ resources/js/data/DataManager.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 
-import {DownloadJob, DownloadWorker} from "../utils/DownloadWorker.js";
+import { DownloadJob, DownloadWorker } from "../utils/DownloadWorker.js";
 import WeeklyMenu from "./WeeklyMenu.js";
 import Config from "./DataConfig.js";
 
@@ -34,22 +34,26 @@ function translateMenuEntries(menuData) {
 function createMenu(dailyMenus) {
   let newMenu = new WeeklyMenu();
   for (let i = 0; i < dailyMenus.length; i++) {
-    let menuForCurrentDay = dailyMenus[i],
-      currentShortDay = menuForCurrentDay[0].day,
-      currentLongDay = getLongWeekday(currentShortDay);
+
+    let menuForCurrentDay, currentLongDay;
+    menuForCurrentDay = dailyMenus[i];
+    if(menuForCurrentDay.length === 0) {
+      continue;
+    }
+    currentLongDay = getLongWeekday(menuForCurrentDay[0].day);
     newMenu[currentLongDay] = translateMenuEntries(menuForCurrentDay);
   }
   return newMenu;
 }
 
 function createUpdatePromise() {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     let worker = new DownloadWorker();
-    worker.addEventListener("finish", function(results) {
+    worker.addEventListener("finish", function (results) {
       menuData = createMenu(results.data);
       resolve();
     });
-    worker.addEventListener("error", function(error) {
+    worker.addEventListener("error", function (error) {
       reject(error.data);
     });
     for (let i = 0; i < Config.WEEKDAYS_SHORT.length; i++) {
