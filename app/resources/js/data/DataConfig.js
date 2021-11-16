ecb3b265b177eadc89218e1d67d8eab53de4ecb3
Jakob Fehle <jakob.fehle@sprachlit.uni-regensburg.de>
2021-11-16 18:02:14 +0100

    Init starter for ws2122

--- app/resources/js/data/DataConfig.js
+++ app/resources/js/data/DataConfig.js
@@ -1,7 +1,7 @@
 /* eslint-env browser */
 
 const Config = {
-    BASE_API_URL: "https://regensburger-forscher.de:9001/mensa/uni/{{day}}",
+    BASE_API_URL: "https://mensa.software-engineering.education/mensa/uni/{{day}}",
     WEEKDAYS_SHORT: ["mo", "di", "mi", "do", "fr"],
     WEEKDAYS_LONG: ["monday", "tuesday", "wednesday", "thursday", "friday"],
 };
